f241415e4740d439038ff3198c7c28fb88f99ffb
Alessandro De Marzo <demarzoad@libero.it>
2023-03-06 20:24:29 +0100

    Added readme

---
+++ readme.md
@@ -0,0 +1,10 @@
+# QUOTES GENERATOR By 1lgrand
+
+- Responsive web design
+- Using API of Quotable [https://docs.quotable.io/docs/api/ZG9jOjQ2NDA2-introduction]
+
+# Screenshot
+
+
+
+# Stylesheet
--- static/script.js
+++ static/script.js
@@ -1,3 +1,7 @@
-function randomQuote(){
-    
+async function randomQuote() {
+    const response = await fetch('https://api.quotable.io/random')
+    const quote = await response.json()
+        
+    console.log(quote.content)
+    console.log(`- ${quote.author}`)
 }
